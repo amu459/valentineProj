d930f2bb2bc13aec0c07ede897abac8d3855c2f8
Amrut Modani <59799686+amu459@users.noreply.github.com>
2026-02-01 22:31:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,5 @@ function growYes() {
 function celebrate() {
     document.getElementById('question').innerText = "YAY! See you on Valentine's! 🥰";
     document.getElementById('buttons').style.display = 'none';
-    //document.getElementById('mainGif').src = <"https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZXd0eDZxcGN3OWx5Z21yZDlrY3h0eDhhY2s0bTRzM3gxNXIzeGtwdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/HJibfnd7xqk5hAMD4v/giphy.gif">;
-    //document.getElementById('mainGif').src = "https://giphy.com/embed/HJibfnd7xqk5hAMD4v"; // Happy cat GIF
-    <iframe src="https://giphy.com/embed/HJibfnd7xqk5hAMD4v" width="480" height="480" style="" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/love-kiss-hearts-HJibfnd7xqk5hAMD4v">via GIPHY</a></p>
     document.getElementById('mainGif').src = "https://media.giphy.com/media/HJibfnd7xqk5hAMD4v/giphy.gif"; // Happy cat GIF
 }
